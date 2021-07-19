53c97372cfdf3289fa759b0446eb083ebc66dfe0
Carlos Devoto <cdevoto@maddogtechnology.com>
2021-07-19 12:41:12 -0400

    Minor tweaks

--- pecs/src/test/java/com/resolute/pecs/PecsTest.java
+++ pecs/src/test/java/com/resolute/pecs/PecsTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -17,46 +19,34 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("unused")
 public class PecsTest {
 
-  @Test
-  public void test_consumer_without_pecs() {
-    Consumer<Animal> consumer1 = (Animal a) -> System.out.println("Consuming " + a);
-    Consumer<Canine> consumer2 = (Canine b) -> System.out.println("Consuming " + b);
-    Consumer<Dog> consumer3 = (Dog c) -> System.out.println("Consuming " + c);
-
-    // Since Canine is a subclass of Animal it should be possible to use a consumer of Animal
-    // objects in order consume Canine objects, but the compiler does not allow it.
-    // processCanineConsumerWithoutPecs(consumer1);
-
-    processCanineConsumerWithoutPecs(consumer2);
-
-    // A consumer of Dog objects cannot consume Canine objects, so the following should never be
-    // allowed.
-    // processCanineConsumerWithoutPecs(consumer3);
-  }
+  /*
+   * The signature for Comparator.comparing looks like this:
+   * 
+   * public static <T, U extends Comparable<? super U>> Comparator<T> comparing( Function<? super T,
+   * ? extends U> keyExtractor)
+   * 
+   * Why is it so complex? To understand this signature, you first need to understand the PECS
+   * principle: Producer Extends, Consumer Super.
+   */
 
   @Test
-  public void test_consumer_with_pecs() {
-    Consumer<Animal> consumer1 = (Animal a) -> System.out.println("Consuming " + a);
-    Consumer<Canine> consumer2 = (Canine b) -> System.out.println("Consuming " + b);
-    Consumer<Dog> consumer3 = (Dog c) -> System.out.println("Consuming " + c);
+  public void intro() {
+    List<Canine> canines =
+        new ArrayList<>(Arrays.asList(new Dog(4), new Wolf(3), new Dog(8), new Wolf(1)));
 
-    processCanineConsumerWithPecs(consumer1);
+    Comparator<Canine> comparator = Comparator.comparing((Animal a) -> a.getId());
 
-    processCanineConsumerWithPecs(consumer2);
+    Collections.sort(canines, comparator);
 
-    // A consumer of Dog objects cannot consume Canine objects, so the following should never be
-    // allowed.
-    // processCanineConsumerWithPecs(consumer3);
-  }
+    assertThat(canines.get(0)).isInstanceOf(Wolf.class);
+    assertThat(canines.get(0).getId()).isEqualTo(1);
+    assertThat(canines.get(1)).isInstanceOf(Wolf.class);
+    assertThat(canines.get(1).getId()).isEqualTo(3);
+    assertThat(canines.get(2)).isInstanceOf(Dog.class);
+    assertThat(canines.get(2).getId()).isEqualTo(4);
+    assertThat(canines.get(3)).isInstanceOf(Dog.class);
+    assertThat(canines.get(3).getId()).isEqualTo(8);
 
-  private void processCanineConsumerWithoutPecs(Consumer<Canine> consumer) {
-    Canine b = new Canine();
-    consumer.accept(b);
-  }
-
-  private void processCanineConsumerWithPecs(Consumer<? super Canine> consumer) {
-    Canine b = new Canine();
-    consumer.accept(b);
   }
 
   @Test
@@ -99,6 +89,48 @@ public class PecsTest {
   private void processCanineProducerWithPecs(Supplier<? extends Canine> supplier) {
     Canine b = supplier.get();
     System.out.println("Produced: " + b);
+  }
+
+  @Test
+  public void test_consumer_without_pecs() {
+    Consumer<Animal> consumer1 = (Animal a) -> System.out.println("Consuming " + a);
+    Consumer<Canine> consumer2 = (Canine b) -> System.out.println("Consuming " + b);
+    Consumer<Dog> consumer3 = (Dog c) -> System.out.println("Consuming " + c);
+
+    // Since Canine is a subclass of Animal it should be possible to use a consumer of Animal
+    // objects in order consume Canine objects, but the compiler does not allow it.
+    // processCanineConsumerWithoutPecs(consumer1);
+
+    processCanineConsumerWithoutPecs(consumer2);
+
+    // A consumer of Dog objects cannot consume Canine objects, so the following should never be
+    // allowed.
+    // processCanineConsumerWithoutPecs(consumer3);
+  }
+
+  @Test
+  public void test_consumer_with_pecs() {
+    Consumer<Animal> consumer1 = (Animal a) -> System.out.println("Consuming " + a);
+    Consumer<Canine> consumer2 = (Canine b) -> System.out.println("Consuming " + b);
+    Consumer<Dog> consumer3 = (Dog c) -> System.out.println("Consuming " + c);
+
+    processCanineConsumerWithPecs(consumer1);
+
+    processCanineConsumerWithPecs(consumer2);
+
+    // A consumer of Dog objects cannot consume Canine objects, so the following should never be
+    // allowed.
+    // processCanineConsumerWithPecs(consumer3);
+  }
+
+  private void processCanineConsumerWithoutPecs(Consumer<Canine> consumer) {
+    Canine b = new Canine();
+    consumer.accept(b);
+  }
+
+  private void processCanineConsumerWithPecs(Consumer<? super Canine> consumer) {
+    Canine b = new Canine();
+    consumer.accept(b);
   }
 
   @Test
@@ -214,12 +246,14 @@ public class PecsTest {
   private void processCanineListActingAsProducerWithoutPecs(List<Canine> list) {
     for (int i = 0; i < list.size(); i++) {
       Canine b = list.get(0);
+      System.out.println("Consuming: " + b);
     }
   }
 
   private void processCanineListActingAsProducerWithPecs(List<? extends Canine> list) {
     for (int i = 0; i < list.size(); i++) {
       Canine b = list.get(0);
+      System.out.println("Consuming: " + b);
     }
   }
 
@@ -236,7 +270,8 @@ public class PecsTest {
 
   private void processCanineListActingAsProducerAndConsumer(List<Canine> list) {
     for (int i = 0; i < list.size(); i++) {
-      Canine b = list.get(0);
+      Canine b = list.get(i);
+      System.out.println("Consuming: " + b);
     }
     list.add(new Canine());
   }
@@ -246,16 +281,18 @@ public class PecsTest {
       List<? extends Canine> list) {
     for (int i = 0; i < list.size(); i++) {
       Canine b = list.get(0);
+      System.out.println("Consuming: " + b);
     }
     // The following line does not compile because we are using extends on a consumer!
-    // list.add(new B());
+    // list.add(new Canine());
   }
 
   // THE FOLLOWING METHOD IS INCORRECTLY DEFINED!
   private void processCanineListActingAsProducerAndConsumerWithSuper(List<? super Canine> list) {
     for (int i = 0; i < list.size(); i++) {
       // The following line does not compile because we are using super on a producer!
-      // B b = list.get(0);
+      // Canine b = list.get(0);
+      // System.out.println("Consuming: " + b);
     }
 
     list.add(new Canine());
@@ -297,6 +334,35 @@ public class PecsTest {
         .sorted()
         .findFirst()
         .orElse(null);
+  }
+
+  public void putting_it_all_together() {
+    List<Canine> canines =
+        new ArrayList<>(Arrays.asList(new Dog(4), new Wolf(3), new Dog(8), new Wolf(1)));
+
+    // Comparator<Canine> comparator1 = comparing_without_pecs((Animal a) -> a.getId());
+    Comparator<Canine> comparator2 = comparing((Animal a) -> a.getId());
+
+    Collections.sort(canines, comparator2);
+
+    assertThat(canines.get(0)).isInstanceOf(Wolf.class);
+    assertThat(canines.get(0).getId()).isEqualTo(1);
+    assertThat(canines.get(1)).isInstanceOf(Wolf.class);
+    assertThat(canines.get(1).getId()).isEqualTo(3);
+    assertThat(canines.get(2)).isInstanceOf(Dog.class);
+    assertThat(canines.get(2).getId()).isEqualTo(4);
+    assertThat(canines.get(3)).isInstanceOf(Dog.class);
+    assertThat(canines.get(3).getId()).isEqualTo(8);
+
+  }
+
+  private <T, U extends Comparable<U>> Comparator<T> comparing_without_pecs(Function<T, U> func) {
+    return (t1, t2) -> func.apply(t1).compareTo(func.apply(t2));
+  }
+
+  private <T, U extends Comparable<? super U>> Comparator<T> comparing(
+      Function<? super T, ? extends U> func) {
+    return (t1, t2) -> func.apply(t1).compareTo(func.apply(t2));
   }
 
 }
